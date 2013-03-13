ae4313af727d64671f65e99d6edb0458942cdee8
melkium <melkium@gmail.com>
2013-03-13 12:29:01 +0100

    javi 13/3

--- Vista/insertar_Trabajador.php
+++ Vista/insertar_Trabajador.php
@@ -1,51 +1,55 @@
 <?php include ('plantilla/cabecera.php'); ?>
 <?php include ('plantilla/menu_admin.php'); ?>  
-
-  <!-- Contenido -->
+<link rel='stylesheet' href='estilos.css'>
+<script type="text/javascript" src="jquery-1.9.1.min.js"></script>
+<script src="funciones.js"></script>
+<!-- Contenido -->
 
 	<div class="row">
-		<form action="alumno_nuevo.php" method="post">
+		<form>
 		  <fieldset>
 			<legend>Insertar Trabajador</legend>
 
 			<div class="row">
-			  <div class="five columns">
-			  
-				<label>DNI</label>
-				<input type="text" id="dni" name="dni" required>
+			  	<div class="five columns">
+				<div><label>DNI</label></div>
+				<input type="text" id="dni" name="dni" class="dni" value="" style="display: inline;">
 				
-				<label>Contraseña</label>
-				<input type="text" id="password" name="password" required>
-				
-				<label>Empresa</label>
-				<input type="text" id="empresa" name="empresa" required>
+				<div><label>Contraseña</label></div>
+				<input type="text" id="password" name="password" class="contraseña" value="" style="display: inline;">
+			
+				<div><label>Empresa</label></div>
+				<input type="text" id="empresa" name="empresa" class="empresa" value="" style="display: inline;">
 
-				<label>Centro de trabajo</label>
-				<input type="text" id="centro" name="centro" required>
+				<div><label>Centro de trabajo</label></div>
+				<input type="text" id="centro" name="centro" class="centrotrabajo" value="" style="display: inline;">
 				
-				<label>Nombre</label>
-				<input type="text" id="nombre" name="nombre" required>
+				<div><label>Nombre</label></div>
+				<input type="text" id="nombre" name="nombre" class="nombre" value="" style="display: inline;">
 
-				<label>Primer apellido</label>
-				<input type="text" id="apellido1" name="apellido1" required>
+				<div><label>Primer apellido</label></div>
+				<input type="text" id="apellido1" name="apellido1" class="apellido1" value="" style="display: inline;">
 				
-				<label>Segundo apellido</label>
-				<input type="text" id="apellido2" name="apellido2" required>	
+				<div><label>Segundo apellido</label></div>
+				<input type="text" id="apellido2" name="apellido2" class="apellido2" value="" style="display: inline;">	
 				
-				<label>Telefono</label>
-				<input type="text" id="telefono" name="telefono" >
+				<div><label>Telefono</label></div>
+				<input type="text" id="telefono" name="telefono" class="telefono" value="" style="display: inline;">
+			</div>
+			
 				
 			  </div>
-			</div>
+
 			<div class="row">
 				<div class="four columns centered">
-					<input class="button left" type="submit" value="Enviar" />
+					<input class="button left" type="submit" value="Enviar" id="boton" />
 					<input class="button right" type="reset" value="Borrar" />
 				</div> 
 			</div>
 		  </fieldset>
 		</form>
 	</div>
+</div>
 
 	
   <!-- Fin del contenido -->
